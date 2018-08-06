3b0f76268b59f1a53249bac52214af5aa7f40163
henryZheng727 <henry.zheng727@gmail.com>
2018-08-06 15:49:39 -0600

    library course component grid finished

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -6,7 +6,7 @@ class Schedule extends Component {
     render() {
         return (
             <div className="schedule">
-                <h1 className="schedule__title"My Schedule></h1>
+                <h1 className="schedule__title">My Schedule></h1>
                 <Gradient/>
             </div>
         )
